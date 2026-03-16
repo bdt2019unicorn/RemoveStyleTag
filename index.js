a66617cacedc84b91c593d236c3cbde37588aa69
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2026-03-17 12:05:23 +1300

    set this up and put on server

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.use(express.json());
 
+function removeStyleTags(htmlCode) {
+  return htmlCode.replace(/<style[^>]*>[\s\S]*?<\/style>/gi, '');
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log('Received GET request at /');
+  res.send('Hello World! Testing');
 });
 
 app.post('/remove-style-tag', (req, res) => {
@@ -13,8 +19,24 @@ app.post('/remove-style-tag', (req, res) => {
     return res.status(400).json({ error: 'htmlCode is required' });
   }
   // Remove <style> tags and their content
-  const cleanedText = htmlCode.replace(/<style[^>]*>[\s\S]*?<\/style>/gi, '');
+  const cleanedText = removeStyleTags(htmlCode);
   res.json({ result: cleanedText });
+});
+
+app.post('/visit-website-and-remove-styles', async (req, res) => {
+  const { url } = req.body;
+  console.log('Received URL:', url);
+  if (!url) {
+    return res.status(400).json({ error: 'url is required' });
+  }
+  try {
+    const response = await axios.get(url);
+    let htmlContent = response.data;
+    htmlContent = removeStyleTags(htmlContent);
+    res.send(htmlContent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
